f8152f2f3fdf36b2cffca08e39e4537629b8edc7
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 19:57:56 +0530

    UPDATED: indian state code information from csv file

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -40,4 +40,35 @@ public class CensusAnalyser {
 					ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
 		}
 	}
+
+	public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+
+		try {
+
+			if (csvFilePath.contains(".txt")) {
+				throw new CensusAnalyserException("File must be in CSV Format",
+						ExceptionType.CENSUS_INCORRECT_FILE_FORMAT);
+			}
+
+			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBeanBuilder<IndiaStateCSV> csvToBeanBuilder = new CsvToBeanBuilder<IndiaStateCSV>(reader);
+			csvToBeanBuilder.withType(IndiaStateCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaStateCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndiaStateCSV> stateCodesCSVIterator = csvToBean.iterator();
+
+			int numberOfEntries = 0;
+			while (stateCodesCSVIterator.hasNext()) {
+				numberOfEntries++;
+				IndiaStateCSV censusData = stateCodesCSVIterator.next();
+			}
+			return numberOfEntries;
+		} catch (IOException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		} catch (RuntimeException e) {
+			throw new CensusAnalyserException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header",
+					ExceptionType.CENSUS_WRONG_DELIMITER_OR_WRONG_HEADER);
+		}
+	}
 }
